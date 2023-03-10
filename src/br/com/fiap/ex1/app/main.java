5be796186c7c47669fd3450371dd415c15aa53ae
Logon Aluno <logonrmlocal@fiap.com.br>
2023-03-10 10:52:03 -0300

    Adicionando Gson

--- src/br/com/fiap/ex1/app/main.java
+++ src/br/com/fiap/ex1/app/main.java
@@ -1,11 +1,15 @@
 package br.com.fiap.ex1.app;
 
 import br.com.fiap.ex1.model.Data;
+import com.google.gson.Gson;
 
 public class main {
     public static void main(String[] args) {
         Data hoje = new Data(28, 2, 2004);
 
-        System.out.println(hoje.mostraData());
+        Gson conversor = new Gson();
+        String s = conversor.toJson(hoje);
+
+        System.out.println(s);
     }
 }
